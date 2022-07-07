a113be1a98330bc579f6f6f4a4750d317fef2b7c
Tina <chen.tina47@gmail.com>
2022-07-07 18:44:59 +0800

    第四次提交上課資料20220705

---
+++ 2-4-2-find/js/index.js
@@ -0,0 +1,9 @@
+                    // >是第一層的li
+const $naviItems = $('#NaviLs > li');
+
+
+$naviItems.hover(function(){
+    $(this).find('ul').stop(true, false).slideDown();
+}, function(){
+    $(this).find('ul').stop(true, false).slideUp();
+});
